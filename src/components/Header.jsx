eb944c6e8acacdc050a7e2cf6c4abf3f76232874
Mostafattaa <mostafaatta733@gmail.com>
2025-03-22 18:50:01 +0200

    home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import { FaBars, FaChevronDown} from "react-icons/fa";
+import { MdDarkMode, MdLightMode , MdLogin  } from "react-icons/md";
+
 import { FaXmark } from "react-icons/fa6";
 import React, { useEffect, useState } from "react";
 import {
@@ -17,39 +19,35 @@ import {
 import { Link } from "react-router-dom";
 
 const navListMenuItems = [
-  { title: "Action" },
-  { title: "Horror" },
-  { title: "Scientific" },
-  { title: "Drama" },
-  { title: "Romantic" },
-  { title: "Classic" },
+  { title: "Top Rated",       path: "/top-rated" },
+  { title: "Now Playing",     path: "/now-playing" }, 
+  { title: "Popular Movies",  path: "/popular" },
+  { title: "Upcoming Movies", path: "/upcoming" },
 ];
 
+
 const navListSeries = [
-  { title: "Ramadan 2025" },
-  { title: "Horror" },
-  { title: "Scientific" },
-  { title: "Drama" },
-  { title: "Romantic" },
-  { title: "Classic" },
+  { title: "Top Rated" },
+  { title: "Now Playing" },
+  { title: "Popular Movies" },
+  { title: "Upcoming Movies" },
+
 ];
 
 const NavDropdown = ({ title, items }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-  const renderItems = items.map(({ title }, key) => (
-    <a href="#" key={key}>
+  const renderItems = items.map(({ title, path }, key) => (
+    <Link to={path} key={key}>
       <MenuItem className="flex items-center gap-3 rounded-lg">
-        <div className="flex items-center justify-center rounded-lg !bg-blue-gray-900 p-2 "></div>
-        <div>
-          <Typography variant="h6" color="blue-gray" className="flex items-center text-sm font-bold">
-            {title}
-          </Typography>
-        </div>
+        <Typography variant="h6" color="blue-gray" className="flex items-center text-sm font-bold">
+          {title}
+        </Typography>
       </MenuItem>
-    </a>
+    </Link>
   ));
+  
 
   return (
     <Menu open={isMenuOpen} handler={setIsMenuOpen} offset={{ mainAxis: 20 }} placement="bottom" allowHover>
@@ -61,13 +59,13 @@ const NavDropdown = ({ title, items }) => {
             onClick={() => setIsMobileMenuOpen((cur) => !cur)}
           >
             {title}
-            <FaChevronDown strokeWidth={2.5} className={`hidden h-3 w-3 transition-transform lg:block ${isMenuOpen ? "rotate-180" : ""}`} />
-            <FaChevronDown strokeWidth={2.5} className={`block h-3 w-3 transition-transform lg:hidden ${isMobileMenuOpen ? "rotate-180" : ""}`} />
+            <FaChevronDown strokeWidth={2.5} className={`hidden h-3 w-3 transition-transform lg:block  ${isMenuOpen ? "rotate-180" : ""}`} />
+            <FaChevronDown strokeWidth={2.5} className={`block h-3 w-3 transition-transform lg:hidden  ${isMobileMenuOpen ? "rotate-180" : ""}`} />
           </ListItem>
         </Typography>
       </MenuHandler>
       <MenuList className="hidden max-w-screen-xl rounded-xl lg:block">
-        <ul className="grid grid-cols-3 gap-y-2 outline-none outline-0">{renderItems}</ul>
+        <ul className="grid grid-cols-2 gap-y-2 outline-none outline-0">{renderItems}</ul>
       </MenuList>
       <div className="block lg:hidden">
         <Collapse open={isMobileMenuOpen}>{renderItems}</Collapse>
@@ -77,14 +75,16 @@ const NavDropdown = ({ title, items }) => {
 };
 
 const NavList = () => (
-  <List className="mt-4 mb-6 p-0 lg:mt-0 lg:mb-0 lg:flex-row lg:p-1">
-    <Typography as={Link} to="/home" variant="small" color="blue-gray" className="font-medium">
-      <ListItem className="flex items-center gap-2 py-2 pr-4">Home</ListItem>
-    </Typography>
+  
+  <List className="mt-4  p-0 lg:mt-0 lg:mb-0 lg:flex-row lg:p-1 ">
+    <Typography as={Link} to="/home" variant="small" color="white" className="font-medium">
+      <ListItem className="flex items-center gap-2 py-2 pr-4  text-white">Home</ListItem>
+    </Typography> 
     <NavDropdown title="Movies" items={navListMenuItems} />
     <NavDropdown title="Series" items={navListSeries} />
+    
     <Typography as={Link} to="/contact" variant="small" color="blue-gray" className="font-medium">
-      <ListItem className="flex items-center gap-2 py-2 pr-4">Contact Us</ListItem>
+      <ListItem className="flex items-center gap-2 py-2 pr-4  text-white">Contact Us</ListItem>
     </Typography>
   </List>
 );
@@ -96,23 +96,44 @@ const Header = () => {
     window.addEventListener("resize", () => window.innerWidth >= 960 && setOpenNav(false));
   }, []);
 
+  const [theme,setTheme]= useState(true)
+
+  function setDarkTheme() {
+    document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
+    setTheme(false)
+    
+  };
+  
+  
+  function setLightTheme() {
+    document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
+    setTheme(true)
+  };
   return (
     <div>
-      <Navbar className="mx-auto max-w-screen-4xl px-4 py-2 ">
-        <div className="flex items-center justify-between text-blue-gray-900">
-          <Typography as={Link} to="/home" variant="h6" className="mr-4 cursor-pointer py-1.5 lg:ml-2">
-            MOVIES
+      <Navbar className="mx-auto max-w-screen-4xl px-4 py-2  bg-gradient-to-tr   from-blue-900 via-cyan-300  to-black  border-none rounded-none" >
+
+        <div className="flex items-center justify-between text-white">
+          <Typography as={Link} to="/home" variant="h6" className="mr-4 cursor-pointer py-1.5 lg:ml-2 hover:text-blue-200">
+            <img src="./movie.png" width={100} alt="" />
+            
           </Typography>
           <div className="hidden lg:block">
             <NavList />
           </div>
           <div className="hidden gap-2 lg:flex">
-            <Button variant="text" size="sm" color="blue-gray">
-              Log In
-            </Button>
-            <Button variant="gradient" size="sm">
-              Sign In
-            </Button>
+          
+          <Typography as={Link} to="/login" variant="h6" className="mr-4 cursor-pointer py-1.5 lg:ml-2">
+            <MdLogin className="text-2xl text-white" />
+          </Typography>
+          <Typography as="span" to="/home" variant="h6" className="mr-4 cursor-pointer py-1.5 lg:ml-2">
+          {theme  ?  <MdDarkMode onClick={setDarkTheme} className="text-2xl text-white "/>
+:            <MdLightMode onClick={setLightTheme} className="text-2xl text-white "/>
+}
+          </Typography> 
+
           </div>
           <IconButton variant="text" color="blue-gray" className="lg:hidden" onClick={() => setOpenNav(!openNav)}>
             {openNav ? <FaXmark className="h-6 w-6" strokeWidth={2} /> : <FaBars className="h-6 w-6" strokeWidth={2} />}
@@ -120,13 +141,14 @@ const Header = () => {
         </div>
         <Collapse open={openNav}>
           <NavList />
-          <div className="flex w-full flex-nowrap items-center gap-2 lg:hidden">
-            <Button variant="outlined" size="sm" color="blue-gray" fullWidth>
-              Log In
-            </Button>
-            <Button variant="gradient" size="sm" fullWidth>
-              Sign In
-            </Button>
+          <div className="flex w-full flex-nowrap  justify-center items-center gap-2 lg:hidden">
+          <Typography as={Link} to="/login" variant="h6" className="mr-4 cursor-pointer py-1.5 lg:ml-2">
+            <MdLogin className="text-3xl text-white" />
+          </Typography>
+            <Typography as="span" to="/home" variant="h6" className="mr-4 cursor-pointer py-1.5 lg:ml-2">
+            {theme  ?  <MdDarkMode onClick={setDarkTheme} className="text-2xl text-white "/>
+:            <MdLightMode onClick={setLightTheme} className="text-3xl text-white "/>
+}          </Typography>
           </div>
         </Collapse>
       </Navbar>

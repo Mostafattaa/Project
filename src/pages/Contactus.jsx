a108deb602657e89ac82b4b47e892f0f5641439e
Mostafattaa <mostafaatta733@gmail.com>
2025-04-16 12:18:30 +0200

    clr

--- src/pages/Contactus.jsx
+++ src/pages/Contactus.jsx
@@ -5,13 +5,13 @@ const Contactus = () => {
   return (
     <div className="min-h-screen  bg-gray-300 dark:bg-gray-900 text-black dark:text-white flex flex-col items-center p-8">
       <h1 className="text-4xl font-bold mb-8"> Feel Free to Contact Us!</h1>
-      <form className="w-full max-w-lg bg-gray-800 p-6 rounded-2xl shadow-xl space-y-6">
+      <form className="w-full max-w-lg bg-gray-700 p-6 rounded-2xl shadow-xl space-y-6">
         <div>
           <label className="block mb-2 text-sm font-medium">Full Name</label>
           <input
             type="text"
             placeholder="Your name"
-            className="w-full px-4 py-2 rounded-xl bg-gray-700 text-white outline-none"
+            className="w-full px-4 py-2 rounded-xl bg-gray-600 text-white outline-none"
           />
         </div>
         <div>
@@ -19,7 +19,7 @@ const Contactus = () => {
           <input
             type="email"
             placeholder="you@example.com"
-            className="w-full px-4 py-2 rounded-xl bg-gray-700 text-white outline-none"
+            className="w-full px-4 py-2 rounded-xl bg-gray-600 text-white outline-none"
           />
         </div>
         <div>
@@ -27,12 +27,12 @@ const Contactus = () => {
           <textarea
             rows="5"
             placeholder="Your message"
-            className="w-full px-4 py-2 rounded-xl bg-gray-700 text-white outline-none"
+            className="w-full px-4 py-2 rounded-xl bg-gray-600 text-white outline-none"
           ></textarea>
         </div>
         <button
           type="submit"
-          className="w-full bg-red-600 hover:bg-red-700 dark:bg-purple-500 dark:hover:bg-purple-700 py-2 rounded-xl font-semibold transition duration-200"
+          className=" p-4 bg-red-600 hover:bg-red-700 dark:bg-purple-500 dark:hover:bg-purple-700 py-2 rounded-xl font-semibold transition duration-200"
         >
           Send Message
         </button>
